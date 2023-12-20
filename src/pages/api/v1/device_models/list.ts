6ef4a40922b70e2f88503f5ee466c90cc139cf15
Evan Sosenko <evan@getseam.com>
2023-12-20 17:37:56 -0600

    Refactor filter logic

--- src/pages/api/v1/device_models/list.ts
+++ src/pages/api/v1/device_models/list.ts
@@ -3,6 +3,7 @@ import { get } from "lodash"
 
 import { getBaseUrl } from "lib/get-base-url.ts"
 import { withRouteSpec } from "lib/middleware/index.ts"
+import type { StoredDeviceModelV1 } from "lib/models.ts"
 import { publicMapDeviceModelV1 } from "lib/public-mappings/device-model-v1.ts"
 
 export default withRouteSpec({
@@ -10,98 +11,85 @@ export default withRouteSpec({
   methods: ["GET", "POST", "OPTIONS"],
   auth: "vercel_protection_bypass_secret",
 } as const)(async (req, res) => {
-  const { include_if, exclude_if } = req.query
-
-  if (include_if?.length === 0) {
-    res.status(200).json({
-      device_models: [],
-    })
-    return
-  }
-
-  let filtered_device_models = [...req.db.device_models]
-
-  if (include_if != null) {
-    filtered_device_models = filtered_device_models.filter((device_model) =>
-      include_if
-        .map((path) => get(device_model, path))
-        .every((v) => v === true),
-    )
-  }
-
-  if (exclude_if != null) {
-    filtered_device_models = filtered_device_models.filter((device_model) =>
-      exclude_if
-        .map((path) => get(device_model, path))
-        .every((v) => v !== true),
-    )
-  }
-
-  if (req.query.main_category) {
-    filtered_device_models = filtered_device_models.filter(
-      (device_model) => device_model.main_category === req.query.main_category,
-    )
-  }
-
-  if (req.query.manufacturer_id) {
-    filtered_device_models = filtered_device_models.filter(
-      (device_model) =>
-        device_model.manufacturer_id === req.query.manufacturer_id,
-    )
-  }
-
-  if (req.query.manufacturer_ids) {
-    filtered_device_models = filtered_device_models.filter(
-      (device_model) =>
-        req.query.manufacturer_ids?.includes(device_model.manufacturer_id),
-    )
-  }
-
-  if (req.query.integration_status) {
-    filtered_device_models = filtered_device_models.filter((device_model) => {
-      const manufacturer = req.db.manufacturers.find(
-        ({ manufacturer_id }) =>
-          device_model.manufacturer_id === manufacturer_id,
-      )
-
-      if (!manufacturer) {
-        throw new Error(
-          `Manufacturer for device model ${device_model.device_model_id} cannot be found`,
-        )
-      }
-
-      return manufacturer.integration === req.query.integration_status
-    })
-  }
-
-  if (req.query.text_search) {
-    filtered_device_models = filtered_device_models.filter((device_model) =>
-      device_model.display_name
-        .toLowerCase()
-        .includes(req.query.text_search?.toLowerCase() ?? ""),
-    )
-  }
+  const device_models = req.db.device_models
 
   res.status(200).json({
-    device_models: filtered_device_models.map((device_model) => {
-      const manufacturer = req.db.manufacturers.find(
-        (m) => m.manufacturer_id === device_model.manufacturer_id,
-      )
+    device_models: device_models
+      .filter((device_model: StoredDeviceModelV1) => {
+        const { exclude_if } = req.query
+        if (exclude_if == null) return true
 
-      if (!manufacturer) {
-        throw new Error(
-          `Manufacturer for device model ${device_model.device_model_id} cannot be found`,
-        )
-      }
-
-      const fake_devicedb_base_url = getBaseUrl(req)
-
-      return publicMapDeviceModelV1({
-        device_model,
-        manufacturer,
-        fake_devicedb_base_url,
-        db: req.db,
+        return exclude_if
+          .map((path) => get(device_model, path))
+          .every((v) => v !== true)
       })
-    }),
+      .filter((device_model: StoredDeviceModelV1) => {
+        const { include_if } = req.query
+        if (include_if == null) return true
+        if (include_if.length === 0) return false
+
+        return include_if
+          .map((path) => get(device_model, path))
+          .every((v) => v === true)
+      })
+      .filter((device_model: StoredDeviceModelV1) => {
+        const { main_category } = req.query
+        if (main_category == null) return true
+
+        return device_model.main_category === main_category
+      })
+      .filter((device_model: StoredDeviceModelV1) => {
+        const { manufacturer_id } = req.query
+        if (manufacturer_id == null) return true
+
+        return device_model.manufacturer_id === manufacturer_id
+      })
+      .filter((device_model: StoredDeviceModelV1) => {
+        const { manufacturer_ids } = req.query
+        if (manufacturer_ids == null) return true
+
+        return manufacturer_ids.includes(device_model.manufacturer_id)
+      })
+      .filter((device_model: StoredDeviceModelV1) => {
+        const { integration_status } = req.query
+        if (integration_status == null) return true
+
+        const manufacturer = req.db.manufacturers.find(
+          ({ manufacturer_id }) =>
+            device_model.manufacturer_id === manufacturer_id,
+        )
+
+        if (manufacturer == null) return false
+
+        return manufacturer.integration === integration_status
+      })
+      .filter((device_model: StoredDeviceModelV1) => {
+        const { text_search } = req.query
+        if (text_search == null) return true
+
+        return device_model.display_name
+          .toLowerCase()
+          .includes(text_search.toLowerCase())
+      })
+      .map((device_model) => {
+        const manufacturer = req.db.manufacturers.find(
+          (m) => m.manufacturer_id === device_model.manufacturer_id,
+        )
+
+        if (!manufacturer) {
+          throw new Error(
+            `Manufacturer for device model ${device_model.device_model_id} cannot be found`,
+          )
+        }
+
+        const fake_devicedb_base_url = getBaseUrl(req)
+
+        return publicMapDeviceModelV1({
+          device_model,
+          manufacturer,
+          fake_devicedb_base_url,
+          db: req.db,
+        })
+      }),
   })
 })
